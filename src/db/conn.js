9b120fa7f2e4eb4588fdc7c08e6e9e68bc79fd2a
U2-Code-Crafter <144172742+U2-Code-Crafter@users.noreply.github.com>
2024-05-08 18:55:10 +0500

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://umerkhalid054:iOMAEGPvhd9P1rtM@cluster0.vrp6bv1.mongodb.net/eshop?retryWrites=true&w=majority").then(() => {
+mongoose.connect("mongodb+srv://umerkhalid054:mdb054@cluster0.vrp6bv1.mongodb.net/eshop?retryWrites=true&w=majority").then(() => {
     console.log("connection is successful");
 }).catch((e) => {
     console.log(e, "No connection");
